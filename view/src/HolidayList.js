f10416f8367c5dfc79426ced895c0d50a6c3da0e
Developer <World@Developers-MacBook-Air.local>
2024-02-18 22:51:21 +0530

    src/HolidayList.js

--- view/src/HolidayList.js
+++ view/src/HolidayList.js
@@ -1,30 +1,63 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const HolidayList = () => {
-  const [holidays, setHolidays] = useState([]);
+  const [holidaysByYear, setHolidaysByYear] = useState({});
 
   useEffect(() => {
-    const fetchHolidays = async () => {
+    const fetchData = async () => {
       try {
-        const response = await fetch('http://localhost:8080/https://indian-holiday-api.onrender.com/api/holidays');
-        const data = await response.json();
-        setHolidays(data);
+        const response = await axios.get('https://cors-anywhere.herokuapp.com/https://indian-holiday-api.onrender.com/api/holidays', {
+          headers: {
+            'x-api-key': 'baf0b635ed05bdbf28929f41a909859df15c8342549e4d8a1a091a10714870e0'
+          }
+        });
+        const data = response.data;
+        const holidaysGroupedByYear = groupHolidaysByYear(data);
+        setHolidaysByYear(holidaysGroupedByYear);
       } catch (error) {
         console.error('Error fetching holidays:', error);
       }
     };
 
-    fetchHolidays();
+    fetchData();
   }, []);
+
+  const groupHolidaysByYear = (holidays) => {
+    const groupedHolidays = {};
+    holidays.forEach((holiday) => {
+      const year = new Date(holiday.date).getFullYear();
+      if (!groupedHolidays[year]) {
+        groupedHolidays[year] = [];
+      }
+      groupedHolidays[year].push(holiday);
+    });
+    return groupedHolidays;
+  };
 
   return (
     <div>
-      <h1>Holiday List</h1>
-      <ul>
-        {holidays.map((holiday, index) => (
-          <li key={index}>{holiday.name} - {new Date(holiday.date).toLocaleDateString()}</li>
-        ))}
-      </ul>
+      {Object.keys(holidaysByYear).map((year) => (
+        <div key={year}>
+          <h2>{year}</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {holidaysByYear[year].map((holiday, index) => (
+                <tr key={index}>
+                  <td>{holiday.name}</td>
+                  <td>{new Date(holiday.date).toLocaleDateString()}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ))}
     </div>
   );
 };
